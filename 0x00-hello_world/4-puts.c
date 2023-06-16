36ba826f5189964fd03f1e15d93989888c1cffc8
Ermias80 <ermiasmaz8069@gail.com>
2023-06-15 19:02:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-
 # include<stdio.h>
-int main(void){
+int main(void)
+{
 /**
  * main-entry point
  * 
  * Return: Always 0 (success)
  */
-	puts("\  Programming is like building a multilingual puzzle");
+	puts("\"  Programming is like building a multilingual puzzle");
 	return (0);
 }
