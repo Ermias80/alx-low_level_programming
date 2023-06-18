c22c1c2eb54ee59ca44ad0e15238d0eea75bd6e8
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-18 11:40:02 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,11 +5,14 @@
  */
 int main(void)
 {
-char letter = 'A';
-while (letter <= 'Z')
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 {
 putchar(letter);
-letter++;
+}
+for (letter = 'A'; letter <= 'Z'; letter++)
+{
+putchar(letter);
 }
 putchar('\n');
 return (0);
