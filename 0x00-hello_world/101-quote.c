3e7e8df5a63551d4d1b4081972872cedf5af59f4
Ermias80 <ermiasmaz8069@gmail.com>
2023-06-17 19:38:46 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/syscall.h>
-
-int main() {
-    const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
-    return 1;
+int main()
+{
+const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+return 1;
 }
 
