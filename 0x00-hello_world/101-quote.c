c45df0c8a00888672ffa584959b2fa2ab227d72e
Ermias80 <ermiasmaz8069@gmail.com>
2023-06-17 20:03:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/syscall.h>
+/**
+ * this is question 8
+ * it return 0
+*/
 int main(void)
 {
- write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 return 1;
 }
 
