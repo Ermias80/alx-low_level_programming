611a5d2d402195614587eb3cbbbeb8715759ff19
Ermias80 <ermiasmaz8069@gmail.com>
2023-06-17 20:08:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <sys/syscall.h>
 /**
- * this is question 8
- * it return 0
+ * main -Entery point
+ * Return: Always 0 (success)
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 return 1;
 }
 
