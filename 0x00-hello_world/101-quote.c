52d9f71ec872e1eb2a744676f4969c4437c2101a
Ermias80 <ermiasmaz8069@gmail.com>
2023-06-17 19:58:45 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/syscall.h>
-int main()
+int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, STDERR_FILENO, message, sizeof(message) - 1);
+ write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 return 1;
 }
 
