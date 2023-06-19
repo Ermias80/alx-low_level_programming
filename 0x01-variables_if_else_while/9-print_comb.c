00d12202367815753e7595d00575c72a9d684506
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 06:48:50 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,14 +5,14 @@
  * main - alphabet in lowercase
  * Return: Always 0 (success)
  */
-int main (void)
+int main(void)
 {
 int num = 0;
 while (num <= 9)
 {
 putchar(num + '0');
 num++;
-if(num != 10)
+if (num != 10)
 {
 putchar(',');
 putchar(' ');
