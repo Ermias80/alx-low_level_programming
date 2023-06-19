a3eb8ff054c1eea3a9fdcc9068f3ba274cf88f3d
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 05:45:56 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@
 int main (void)
 {
 int num;
-for( num = '0'; num < '10'; num++)
+for( num = '0'; num <='9'; num++)
 {
 putchar(num);
 if(num!= '9')
 {
-putchar(",");
-putchar(" ");
+putchar(',');
+putchar(' ');
 }
 }
 putchar("\n");
