3b448583f4727f404c0862405501865b8f15ea8a
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 10:39:35 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(void)
+{
+int num1, num2;
+for (num1 = 0; num1 <= 99; num1++)
+{
+for (num2 = num1; num2 <= 99; num2++)
+{
+int tens1 = num1 / 10;
+int ones1 = num1 % 10;
+int tens2 = num2 / 10;
+int ones2 = num2 % 10;
+if (num1 <= num2)
+{
+putchar(tens1 + '0');
+putchar(ones1 + '0');
+putchar(' ');
+putchar(tens2 + '0');
+putchar(ones2 + '0');
+if (num1 != 99 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
