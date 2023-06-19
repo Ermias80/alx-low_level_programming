87339ee4cebd50c48d30a10640743a3e9f2228e6
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 11:06:37 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,23 +10,16 @@ for (num1 = 0; num1 <= 99; num1++)
 {
 for (num2 = num1+1; num2 <= 99; num2++)
 {
-int tens1 = num1 / 10;
-int ones1 = num1 % 10;
-int tens2 = num2 / 10;
-int ones2 = num2 % 10;
-if (num1 <= num2)
-{
-putchar(tens1 + '0');
-putchar(ones1 + '0');
+putchar((num1 / 10) + '0');
+putchar((num2 % 10) + '0');
 putchar(' ');
-putchar(tens2 + '0');
-putchar(ones2 + '0');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 if (num1 != 98 && num2 != 99)
 {
 continue;
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
