877258e59451503550b0271b5dad779b8ac52b3f
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 08:52:57 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - alphabet in lowercase
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int num1, num2;
+int num1, num2, num3;
 for (num1 = '0'; num1 <= '9'; num1++)
-{
-for (num2 = num1 + 1; num2 <= '9'; num2++)
-{
-if (num2 != num1)
+for (num2 = num1+1; num2 <= '9'; num2++)
+for (num3 = num2+1; num2 <= '9'; num3++)
+if ((num2 != num1) != num2)
 {
 putchar(num1);
 putchar(num2);
-if (num1 == '8' && num2 == '9')
+putchar(num3);
+if (num1 != '7' && num2 != '8')
 continue;
 putchar(',');
 putchar(' ');
-
+}
 }
 }
 }
