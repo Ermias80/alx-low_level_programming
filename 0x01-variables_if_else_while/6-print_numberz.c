d8ce107eb6507688303ae423292ea8f9fcbbbae0
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 01:51:54 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int num 10;
+for (int i=0 ;i< num; i++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
