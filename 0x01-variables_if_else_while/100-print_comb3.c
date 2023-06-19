e02e2247d2e2852f8c437574bb1540e36071e890
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-19 07:16:24 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int num1, num2 ;
+for (num1 =0; num1 <=9; num1++ )
+{
+for (num2 =num1+1; num2 <=9; num2++)
+{
+if (num2 != num1)
+{
+putchar(num1);
+putchar(num2);
+if (num2 == '8' && num1 == '9')
+{
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
