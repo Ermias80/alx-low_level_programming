51965cbd81dda21564e74ae811895fc8fff598a0
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-20 18:05:25 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
-gcc 0-putchar.c -o 0-putchar
+#include "main.h"
+/**
+ * main - alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
