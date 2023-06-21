27a86d6b49c278df48a7ef59ed6124d1c3473086
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 11:02:55 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * times_table - alphabet in lowercase
+ * jack_bauer - alphabet in lowercase
  * Return: 1 for lowaer character, 0 for the rest
  */
 void jack_bauer(void)
@@ -8,9 +8,9 @@ void jack_bauer(void)
 int a, b, c, d;
 for (a = 0; a <= 2; a++)
 {
-for (b =0; b <= 9; b++)
+for (b = 0; b <= 9; b++)
 {
-if ((a <= 1 && b <=9) || (a <= 2 && b <= 3))
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0; c <= 5; c++)
 {
