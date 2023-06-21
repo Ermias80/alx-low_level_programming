2e7c6cebc6a6ce0f2721c6a76846c5a5e63c88c5
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 12:26:22 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * times_table - alphabet in lowercase
- * @c: the character in ASCII code
  * Return: 1 for lowaer character, 0 for the rest
  */
 void times_table(void)
@@ -15,7 +14,7 @@ z = x * y;
 if (z > 9)
 {
 u = 2 % 10;
-d = (z - u)/10;
+d = (z - u) / 10;
 _putchar(44);
 _putchar(32);
 _putchar(d + '0');
