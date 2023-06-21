6129c3148d990510438c3fd15c1ea7797d4f7256
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-20 18:11:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char leater = 'a';
+while(leater <= 'z')
+{
+_putchar('leater');
+leater++;
+}
+_putchar('\n');
+return (0);
+}
