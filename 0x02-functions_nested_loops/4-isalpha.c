b3ab069faebeb980eeaa8ee61bddfb665ba4b3dd
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-20 19:30:56 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c >= 64 && c <= 90))
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+_putchar('\n');
+}
