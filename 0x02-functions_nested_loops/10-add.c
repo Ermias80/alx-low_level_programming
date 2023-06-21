589b1a1f546ef940d87c37eb4ffff1a3a7cbe074
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 13:34:47 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _islower - alphabet in lowercase
- * @c: the character in ASCII code
+ * add - add two numbers
+ * @n1: the first number
+ * @n2: the second number 
  * Return: 1 for lowaer character, 0 for the rest
  */
 int add(int n1, int n2)
