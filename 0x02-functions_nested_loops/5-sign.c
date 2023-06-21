11b3965a83ff52b53ce607059c3789bb69ff0aea
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-20 19:47:07 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+int print_sign(int n)
+{
+if (n > =)
+{
+_putchar('+');
+_putchar(',')
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+_putchar(',')
+return (0);
+}
+else
+{
+_putchar('-');
+_putchar(',')
+return (-1);
+}
+_putchar('\n');
+}
