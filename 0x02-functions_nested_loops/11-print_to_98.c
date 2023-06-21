8b46d59ef5baf2efd2f06e24cce0a02e87784222
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 13:53:30 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _islower - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <=89; n++)
+{
+if (n == 98)
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d", n);
+}
+}
+}
