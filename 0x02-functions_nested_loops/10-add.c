8d159c4f55b2c55b25b7692015fe0d40d57d7ac8
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 13:31:07 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
