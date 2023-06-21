9657dd05f9f3bda43728bf407c8cd29a47af959b
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 13:36:40 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - add two numbers
  * @n1: the first number
- * @n2: the second number 
+ * @n2: the second number
  * Return: 1 for lowaer character, 0 for the rest
  */
 int add(int n1, int n2)
