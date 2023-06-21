8d532514a0d10b93a14484946200a25a1a442b94
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-21 08:12:09 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _islower - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+int _abs(int)
+{
+int num = -1;
+int j;
+if (j > 0)
+{
+_putchar(j);
+}
+else if (j == 0)
+{
+_putchar(j);
+}
+else
+{
+_putchar(j*num);
+}
+_putchar('\n');
+return (0);
+}
