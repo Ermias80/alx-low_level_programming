a4818ca39eb2358b172c0b01d7b33b9b6f2c57dc
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 17:55:56 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,9 +3,9 @@
  * mul - alphabet in lowercase
  * @a: the first num to multiplay
  * @b: the second number
- * Return: 1 for lowaer character, 0 for the rest
+ * Return: the multiplication rsult
  */
-int mul(int a, int b);
+int mul(int a, int b)
 {
 return (a * b);
 }
