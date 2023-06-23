383056117c2f94f44176c221a8c8eb4a574baa53
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 17:47:24 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mu - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+int mul(int a, int b);
+{
+int mul = 0;
+num = (a * b);
+putchar(num + '0');
+}
+putchar('\n');
+return (0);
+}
