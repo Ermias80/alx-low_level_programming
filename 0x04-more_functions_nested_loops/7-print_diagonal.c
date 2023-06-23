248b265e5343f3bf4d455ef354d2c82efc69afd9
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 19:37:32 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - drow the diagram
+ * Return: empity
+ */
+void print_diagonal(int n)
+{
+int x, y;
+if (n <= 0)
+{
+putchar('\n');
+}
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < x; y++)
+{
+putchar(32);
+}
+putchar(92);
+putchar('\n');
+}
+}
