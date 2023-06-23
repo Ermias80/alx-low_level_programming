4d7eb4dc29dc95c4ecb9ec47258f2262e34de39a
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 18:13:43 -0700

    print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 /**
- * _islower - alphabet in lowercase
- * @c: the character in ASCII code
- * Return: 1 for lowaer character, 0 for the rest
+ * print_most_numbers - alphabet in lowercase
+ * Return: list of number
  */
 void print_most_numbers(void)
 {
 int n;
 for (n = 0; n <= 9; n++)
 {
-if (n == 2 && n == 4)
+if (n == 2 || n == 4)
 {
 continue;
 }
