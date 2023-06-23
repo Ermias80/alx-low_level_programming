fc09784fdf61783f2c008c197e70c0295e1bc782
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 19:50:39 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_square - print the squear
+ * @size: the size of sqer
+ * Return: empity
+ */
+void print_square(int size)
+{
+int x, y;
+if (size < 0)
+{
+putchar('\n');
+}
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+putchar(35);
+}
+putchar()'\n';
+}
+}
