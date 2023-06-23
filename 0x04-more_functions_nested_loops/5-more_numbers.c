688be41ae19192a47ff38d244fb55e4a7d840652
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 19:01:06 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,15 @@
 void more_numbers(void)
 {
 int i,j;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (y > 9)
+if (j > 9)
 {
-putchar((j/10) +'0');
+putchar((j / 10) +'0');
 }
-putchar((i%10) + '0');
+putchar((i % 10) + '0');
 }
 putchar('\n');
 }
