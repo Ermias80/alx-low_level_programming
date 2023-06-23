60ec244670be478c5038b435dc7b8c9dc3a12688
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 18:31:41 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,8 +5,8 @@
  */
 void print_most_numbers(void)
 {
-int n =0;
-for (; n <= 9; n++)
+int n;
+for (n = 0; n <= 9; n++)
 {
 if (n == 2 || n == 4)
 {
@@ -15,6 +15,7 @@ continue;
 else
 {
 _putchar(n + '0');
+}
 }
 _putchar('\n');
 }
