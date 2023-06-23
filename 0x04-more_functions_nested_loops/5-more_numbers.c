a8eda1ae5229dcaf4230b7aa0689186466b4e601
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-22 19:02:52 -0700

    -more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * _islower - alphabet in lowercase
- * @c: the character in ASCII code
+ * more_numbers - alphabet in lowercase
  * Return: 1 for lowaer character, 0 for the rest
  */
 void more_numbers(void)
