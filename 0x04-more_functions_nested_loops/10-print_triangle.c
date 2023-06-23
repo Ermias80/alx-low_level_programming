84511be47f8135bc50528ead8b80f34b99444120
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-23 10:24:33 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _islower - alphabet in lowercase
+ * @c: the character in ASCII code
+ * Return: 1 for lowaer character, 0 for the rest
+ */
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
