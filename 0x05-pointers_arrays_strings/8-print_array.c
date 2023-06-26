8efcaa36f6dff5319310fc910db96cafdb6e51a6
Ermias80 <ermiasaz8069@gmail.com>
2023-06-26 15:37:42 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - print a string
+ * @s: the starting to print
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int j;
+for (j = 0; j < n; j++)
+{
+printf("%d", a[j]);
+if (j != (n - 1))
+{
+printf(",");
+}
+}
+printf("\n");
+}
