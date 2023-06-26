14d31e1e0b1563a4eea6da5743552ffb7bb69b67
Ermias80 <ermiasaz8069@gmail.com>
2023-06-26 14:51:31 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print a string
+ * @str: the starting to tride
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+int j = 0;
+while (str[j] != '\0')
+{
+j++;
+}
+for (i = 0;; i < j ; i += 2)
+{putchar(str[i]);
+}
+putchar('\n');
+}
