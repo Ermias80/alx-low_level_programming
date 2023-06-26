eac628e932001c1961cf7178340aac39dbffb0c2
Ermias80 <ermiasaz8069@gmail.com>
2023-06-26 14:37:10 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,8 +12,9 @@ char tmp;
 while (s[index++])
 len++;
 for (index = len - 1; index >= len / 2; index--)
+{
 tmp = s[index];
 s[index] = s[len - index -1];
-s(len - index) = tmp;
+s(len - index - 1) = tmp;
 }
 }
