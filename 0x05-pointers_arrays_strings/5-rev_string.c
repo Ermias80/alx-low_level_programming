a447631687bf2ac74315b9850e2c67ce2c4ee6e9
Ermias80 <ermiasaz8069@gmail.com>
2023-06-26 13:56:49 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - print a string
+ * @s: the starting to print
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int len = 0;
+index = 0;
+char tmp;
+while (s[index++])
+len++;
+for (index = len - 1; index >= len/2; index--)
+	s[index] = s[len - index -1];
+s(un -index) = tmo;
+}
+}
