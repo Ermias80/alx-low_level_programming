01456c003658c2436da146d678a35de7fd92b183
Ermias80 <ermiasmaz8069@gmail.co>
2023-06-26 09:44:55 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * _strlen - to return chake the lengeth of the strlen
+ * @s : the value of sterlen
+ * Return: leangth
+ */
+int _strlen(char *s)
+{
+s_t lengeth = 0;
+while (*s++)
+length++;
+return (length);
+}
