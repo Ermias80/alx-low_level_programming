e74e30268a813bd73bff3d1fe517fbc70c495182
Ermias8080 <ermiasmaz8080@gmail.com>
2023-06-29 16:17:08 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _strcmp - compute a strings
- * @s1: the fierst string
- * @s2: the second string
+ * @s1: input value
+ * @s2: input value
  * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
-int i; 
+int i;
 i = 0;
 while (s1[i] != '\0' && s2[i] != '\0')
 {
