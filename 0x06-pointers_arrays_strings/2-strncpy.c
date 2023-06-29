29fb7ea867921e2abbb8499f2a1d1b03f630be77
Ermias8080 <ermiasmaz8080@gmail.com>
2023-06-29 15:22:05 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcat - to concatenate the strings
+ * @dest: the fierst string
+ * @src: the second string
+ * Return: a pointer to a resulting
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src [j];
+j++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+return (dest)
+};
