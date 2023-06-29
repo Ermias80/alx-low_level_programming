c17e08e718fd5f85ba252cbb57558eefec7f2578
Ermias80 <ermiasaz8069@gmail.com>
2023-06-29 14:51:51 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,9 +3,10 @@
  * _strcat - to concatenate the strings
  * @dest: the fierst string
  * @src: the second string
+ * @n: the therd 
  * Return: a pointer to a resulting
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 int i, j;
 i = 0;
