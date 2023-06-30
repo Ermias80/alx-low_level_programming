2b9a4872d60f18a93963bfc9edbf8ba49b980746
Ermias8069 <ermiasmaz8069@gmail.com>
2023-06-29 17:18:38 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * string_toupper - to concatenate the strings
- * @n: the fierst string
- * Return: a pointer to a resulting
+ * string_toupper - cheng all lower case in to uper
+ * @n: the string to bechenged
+ * Return: A pointer
  */
 char *string_toupper(char *n)
 {
 int i = 0;
-while (n[i] != '\0')
+while (n[i])
 {
 if (n[i] >= 'a' && n[i] <= 'z')
 n[i] = n[i] -32;
