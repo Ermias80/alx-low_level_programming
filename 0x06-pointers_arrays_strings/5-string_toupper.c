4e565ad43360e74de82f9fe1017a2a496583a9f7
Ermias8069 <ermiasmaz8069@gmail.com>
2023-06-29 17:01:05 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - to concatenate the strings
+ * @dest: the fierst string
+ * @src: the second string
+ * Return: a pointer to a resulting
+ */
+*string_toupper(char *)
+{
+int i = 0;
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+{
+n[i] = n[i] -32;
+i++;
+}
+}
+return (n);
+}
