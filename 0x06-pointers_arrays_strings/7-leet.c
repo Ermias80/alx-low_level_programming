b89cbe7414b84dfcb73c037f9f589baf016698d0
Ermias8069 <ermiasmaz8069@gmail.com>
2023-06-29 18:52:01 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,24 @@
 #include "main.h"
-/**
- * _leet - encodes a string into Leet Speak
+/* *
+ * leet - encodes a string into Leet Speak
  * @n: the input string
  * Return: a pointer to the resulting string
  */
 char *leet(char *n)
 {
-    int i, j;
-    char s1[] = "aAeEoOtTlL";
-    char s2[] = "4433007711";
-
-    for (i = 0; n[i] != '\0'; i++)
-    {
-        for (j = 0; j < 11; j++)
-        {
-            if (n[i] == s1[j])
-            {
-                n[i] = s2[j];
-                break;
-            }
-        }
-    }
-
-    return n;
+int i, j;
+char s1[] = "aAeEoOtTlL";
+char s2[] = "4433007711"; 
+for (i = 0; n[i] != '\0'; i++)
+{
+for (j = 0; j < 11; j++)
+{
+if (n[i] == s1[j])
+{
+n[i] = s2[j];
+break;
 }
-
+}
+}
+return n;
+}
