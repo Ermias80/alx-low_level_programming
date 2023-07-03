073fe9d0838d93f82165f2b8df4c3dabd790aa30
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-03 16:15:16 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,18 +7,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, n, value, check;
+unsigned int i, n, value;
 value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-check = 0;
 for (n = 0; accept[n] != '\0'; n++)
 {
 if (accept[n] == s[i])
 {
 value++;
-check = 1;
+break; // Exit the inner loop once a matching character is found.
 }
 }
+if (accept[n] == '\0')
+{
+break; // Exit the outer loop.
 }
+}
+return value;
 }
