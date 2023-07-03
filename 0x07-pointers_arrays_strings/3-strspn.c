3f2fabdb9372daf3156566d90498cd086761344b
Ermias8069 <ermiasmaz8069@gmail.com>
2023-07-03 15:43:27 -0700

    -strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strspn - enter pointer
+ * @s: the character in ASCII code
+ * @accept: inpute
+ * Return: always 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, n, value, chake;
+vlaue = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+chake = 0;
+for (n = 0; accept[n] != '\0'; n++)
+if (accept[n] == s[i])
+{
+value++;
+chake = 1;
+}
+}
