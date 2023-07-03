e754f0b42cd593e66e4ef444ef5fe620f88a62ba
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-03 16:06:51 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,17 +7,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, n, value, chake;
-vlaue = 0;
+unsigned int i, n, value, check;
+value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-chake = 0;
+check = 0;
 for (n = 0; accept[n] != '\0'; n++)
 {
 if (accept[n] == s[i])
 {
 value++;
-chake = 1;
+check = 1;
 }
 }
 }
