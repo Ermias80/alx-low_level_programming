bdd99badb6649bc455da45489be0483d86465783
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-05 14:05:26 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,16 +12,16 @@ return (_sqrt(n, 1));
 }
 /**
  * _sqrt -  prints a string in reverse
- * @n: the string to be reverse
- * @i: the s
- * Return: nothing
+ * @n: number to calculate
+ * @i: iterate number
+ * Return: the natural squer root
  */
 int _sqrt(int n, int i)
 {
-	int sqrt = i * i;
-	if (sqrt > n)
-		return (-1);
-	if (sqrt == n)
-		return (i);
-	return (_sqrt(n, i + 1));
+int sqrt = i * i;
+if (sqrt > n)
+return (-1);
+if (sqrt == n)
+return (i);
+return (_sqrt(n, i + 1));
 }
