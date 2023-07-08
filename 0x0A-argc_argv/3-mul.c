523dcafd08f22b783ee404c8c6ddb9ee682f4df8
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-07 18:23:22 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - print the multiplication of two arg  numbers
+ * main - print the multiplication of two arg numbers
  * @argc: argument counter
  * @argv: aregument vector
  * Return: Always zero
  */
 int main(int argc, char *argv[])
 {
-        int n1 = 0, n2 = 0;
-	if (argc == 3)
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
-	}
-	else
-	{
-		printf("error\n");
-		return (1);
-	}
-        return (0);
+int n1 = 0, n2 = 0;
+if (argc == 3)
+{
+n1 = atoi(argv[1]);
+n2 = atoi(argv[2]);
+printf("%d\n", n1 * n2);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+return (0);
 }
