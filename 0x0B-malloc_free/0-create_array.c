d0c4e5e84c428394935c35ba133ae57964bb6388
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-11 03:13:23 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@ char *create_array(unsigned int size, char c)
 {
 unsigned int n;
 char *arr;
-arr = malloc(sizeof(char)*size);
+arr = malloc(sizeof(char)* size);
 if (size == 0 || arr == NULL)
 {
 return (NULL);
 }
-n=0;
+n = 0;
 while (n < size)
 {
 arr[n] = c;
