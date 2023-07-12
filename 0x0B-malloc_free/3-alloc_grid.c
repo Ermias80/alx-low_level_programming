1cdab51bb733d9cab05d639a3d42091c7ce9aadf
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-12 10:36:18 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,6 @@ return (NULL);
 grid = malloc(sizeof(int *) * height);
 if (grid == NULL)
 return (NULL);
-free(grid);
 for (i = 0; i < height; i++)
 {
 grid[i] = malloc(sizeof(int) * width);
@@ -29,14 +28,13 @@ if (grid[i] == NULL)
 for (j = 0; j < i; j++)
 {
 free(grid[j]);
+}
 free(grid);
 return (NULL);
-}
 }
 for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
-free(grid);
 }
 }
 return (grid);
