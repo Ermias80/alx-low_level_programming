5b1f636a8a1e580846d16bd258bfcdf0a6b06625
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-12 15:59:30 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -21,69 +21,73 @@ char *word_start = NULL;
 in_word = 0;
 
 if (str == NULL || strlen(str) == 0)
-return (NULL);
+        return (NULL);
 
-while (str[i] != '\0')
-{
-if (str[i] != ' ' && !in_word)
-{
-in_word = 1;
-word_count++;
-}
-else if (str[i] == ' ')
-{
-in_word = 0;
-}
-i++;
-}
-words = (char **)malloc((word_count + 1) * sizeof(char *));
-if (words == NULL)
-return (NULL);
-i = 0;
-while (str[i] != '\0')
-{
-if (str[i] != ' ' && !in_word)
-{
-in_word = 1;
-word_start = &str[i];
-}
-else if (str[i] == ' ' && in_word)
-{
-in_word = 0;
-word_length = &str[i] - word_start;
-words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-if (words[word_index] == NULL)
-{
-for (j = 0; j < word_index; j++)
-{
-free(words[j]);
-}
-free(words);
-return (NULL);
-}
-strncpy(words[word_index], word_start, word_length);
-words[word_index][word_length] = '\0';
-word_index++;
-}
-i++;
-}
-if (in_word)
-{
-word_length = &str[i] - word_start;
-words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
-if (words[word_index] == NULL)
-{
-for (j = 0; j <= word_index; j++)
-{
-free(words[j]);
-}
-free(words);
-return (NULL);
-}
-strncpy(words[word_index], word_start, word_length);
-words[word_index][word_length] = '\0';
-word_index++;
-}
-words[word_index] = NULL;
-return (words);
+    while (str[i] != '\0')
+    {
+        if (str[i] != ' ' && !in_word)
+        {
+            in_word = 1;
+            word_count++;
+        }
+        else if (str[i] == ' ')
+        {
+            in_word = 0;
+        }
+        i++;
+    }
+
+    words = (char **)malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
+        return (NULL);
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] != ' ' && !in_word)
+        {
+            in_word = 1;
+            word_start = &str[i];
+        }
+        else if (str[i] == ' ' && in_word)
+        {
+            in_word = 0;
+            word_length = &str[i] - word_start;
+            words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+            if (words[word_index] == NULL)
+            {
+                for (j = 0; j < word_index; j++)
+                {
+                    free(words[j]);
+                }
+                free(words);
+                return (NULL);
+            }
+            strncpy(words[word_index], word_start, word_length);
+            words[word_index][word_length] = '\0';
+            word_index++;
+        }
+        i++;
+    }
+
+    if (in_word)
+    {
+        word_length = &str[i] - word_start;
+        words[word_index] = (char *)malloc((word_length + 1) * sizeof(char));
+        if (words[word_index] == NULL)
+        {
+            for (j = 0; j <= word_index; j++)
+            {
+                free(words[j]);
+            }
+            free(words);
+            return (NULL);
+        }
+        strncpy(words[word_index], word_start, word_length);
+        words[word_index][word_length] = '\0';
+        word_index++;
+    }
+    
+    words[word_index] = NULL;
+    return (words);
 }
