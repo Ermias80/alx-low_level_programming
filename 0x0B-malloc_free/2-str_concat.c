95781657870a3e529942db97bffccdac3bab6b26
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-12 05:38:35 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,38 +3,42 @@
 #include <stdlib.h>
 
 /**
- * *str_concat - check the code for ALX School students.
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
  *
- * Return: Always 0.
+ * Return: A pointer to the concatenated string.
  */
 char *str_concat(char *s1, char *s2)
 {
-int i, j;
-char *coo;
-if (s1 == NULL)
-	s1 = "";
-if (s2 == NULL)
-	s2 = "";
-i = j = 0;
-while (s1[i] != '\0')
-{
-	i++;
-}
-while (s2[j] != '\0')
-{
-	j++;
-}
-coo = malloc(sizeof(char) * (i + j + 1));
-if (coo == NULL)
-	return (NULL);
-while (s1[i] != '\0')
-{
-	coo[i] = s2[i];
-}
-while (s2[j] != '\0')
-{
-	coo[j] = s2[j];
-}
-coo[j] = '\0';
-return (coo);
+    int i, j;
+    char *coo;
+
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    i = j = 0;
+
+    while (s1[i] != '\0')
+        i++;
+
+    while (s2[j] != '\0')
+        j++;
+
+    coo = malloc(sizeof(char) * (i + j + 1));
+
+    if (coo == NULL)
+        return (NULL);
+
+    for (i = 0; s1[i] != '\0'; i++)
+        coo[i] = s1[i];
+
+    for (j = 0; s2[j] != '\0'; j++, i++)
+        coo[i] = s2[j];
+
+    coo[i] = '\0';
+
+    return (coo);
 }
