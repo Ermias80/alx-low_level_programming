d58fdf085d2af87a6c2acf04c49bc5ac158810d0
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-12 05:26:45 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,10 +3,38 @@
 #include <stdlib.h>
 
 /**
- * main - check the code for ALX School students.
+ * *str_concat - check the code for ALX School students.
  *
  * Return: Always 0.
  */
 char *str_concat(char *s1, char *s2)
 {
-
+int i, j;
+char *coo;
+if (s1 == NULL)
+	s1 = "";
+if (s2 == NULL)
+	s2 = "";
+i = j = 0;
+while (s1[i] != '\0')
+{
+	i++;
+}
+while (s2[j] != '\0')
+{
+	j++;
+}
+coo = malloc(sizeof(char) * (i + j + 1));
+if (coo == NULL)
+	return (NULL);
+while (s1[i] != '\0')
+{
+	coo[i] = s2[i];
+}
+while (s2[j] != '\0')
+{
+	coo[j] = s2[j];
+}
+coo[j] = '\0';
+return (coo);
+}
