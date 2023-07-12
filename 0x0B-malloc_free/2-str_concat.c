b2314a56d58ecb0054a134cdf37c35df78ef203b
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-12 05:48:17 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,27 +18,18 @@ if (s1 == NULL)
         s1 = "";
 if (s2 == NULL)
         s2 = "";
-
 i = j = 0;
-
 while (s1[i] != '\0')
         i++;
-
 while (s2[j] != '\0')
         j++;
-
 coo = malloc(sizeof(char) * (i + j + 1));
-
 if (coo == NULL)
         return (NULL);
-
 for (i = 0; s1[i] != '\0'; i++)
         coo[i] = s1[i];
-
 for (j = 0; s2[j] != '\0'; j++, i++)
         coo[i] = s2[j];
-
 coo[i] = '\0';
-
 return (coo);
 }
