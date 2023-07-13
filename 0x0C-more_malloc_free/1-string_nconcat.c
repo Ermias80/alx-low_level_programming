3b337feb393fec2ddc43c9ad16785a50e2afad35
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-13 10:45:00 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * string_nconcat - check the code
  *@s1: the first string
@@ -14,11 +13,17 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 unsigned int i = 0, l = 0;
 char *coo;
 if (s1 == NULL)
-	s1 = "";
+{
+s1 = "";
+}
 if (s2 == NULL)
-	s2 = "";
+{
+s2 = "";
+}
 for (i = 0; s1[i]; i++)
+{
 l++;
+}
 coo = malloc(sizeof(char) * l + 1);
 if (coo == NULL)
 {
@@ -26,9 +31,13 @@ return (NULL);
 }
 l = 0;
 for (i = 0; s1[i]; i++)
+{
 coo[l++] = s1[i];
+}
 for (i = 0; s2[i] && i < n; i++)
+{
 coo[l++] = s2[i];
+}
 coo[l] = '\0';
 return (coo);
 }
