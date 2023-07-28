2b3c00d273ed94176e2934f9597775074b32dfcc
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-27 19:03:21 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -6,7 +6,6 @@
 /**
 * _strlen - finds the length of a string
 * @str: string to find the length of
-*
 * Return: length of string
 */
 unsigned int _strlen(char *str)
@@ -15,7 +14,7 @@ unsigned int _strlen(char *str)
 
 
    for (i = 0; str[i]; i++)
-	   ;
+	;
    return (i);
 }
 
@@ -24,7 +23,6 @@ unsigned int _strlen(char *str)
 * add_node_end - adds a new node to the end of linked list
 * @head: double pointer to a linked list
 * @str: string to add to the new node
-*
 * Return: pointer to the new node
 */
 list_t *add_node_end(list_t **head, const char *str)
