4a490fd1d11c361bba087ddc91403223985f9d97
Ermias80 <ermiasmaz8069@gmail.com>
2023-07-27 19:29:34 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,6 @@
 size_t print_list(const list_t *h)
 {
     size_t count = 0;
-
     while (h)
     {
         if (h->str == NULL)
@@ -21,9 +20,8 @@ size_t print_list(const list_t *h)
         {
             printf("[%u] %s\n", h->len, h->str);
         }
-        h = h->next;
-        count++;
+    h = h->next;
+    count++;
     }
-
     return (count);
 }
