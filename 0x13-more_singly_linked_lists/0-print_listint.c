9f466df0370b52ef2aff1507200627b5ed394dca
Ermias80 <ermiasmaz8069@gmail.com>
2023-08-01 06:43:08 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - singly linked list
- * @h: integer
+ * print_listint - print all the ellemrnts 
+ * @h: linked list of type list int 
  * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
+ * @node_count: it count the nood
+ * Return: Number of nood
  */
 size_t print_listint(const listint_t *h)
 {
 size_t node_count = 0;
-while (h != NULL) 
+while (h != NULL)
 {
         printf("%d\n", h->n);
 	h = h->next;
         node_count++;
-    }
+}
 
     return node_count;
 }
