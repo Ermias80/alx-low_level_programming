8858b2ae671d2b86f684a0aeef20b2fe3ab469c7
Ermias80 <ermiasmaz8069@gmail.com>
2023-08-01 06:48:40 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,8 +3,6 @@
 /**
  * print_listint - print all the ellemrnts 
  * @h: linked list of type list int 
- * @next: points to the next node
- * @node_count: it count the nood
  * Return: Number of nood
  */
 size_t print_listint(const listint_t *h)
