5e4ead2f13999b15b3aaec223c29ec9f7f33abf4
Ermias80 <ermiasmaz8069@gmail.com>
2023-10-02 13:46:19 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	B = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	M = write(fd, text_content, len);
+	M = write(B, text_content, len);
 
 	if (B == -1 || M == -1)
 		return (-1);
