a3a1c9ff56e7bb9c1c40fc307c765cea15036cb8
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-21 15:16:58 -0700

    all

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: head of the list
+ * Return: no return
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
+}
