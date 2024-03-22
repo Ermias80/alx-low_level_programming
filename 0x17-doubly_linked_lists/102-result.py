a9cf0661fa0402294f5eb3fa082194a29217deb1
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-21 18:25:30 -0700

    all

--- 0x17-doubly_linked_lists/102-result.py
+++ 0x17-doubly_linked_lists/102-result.py
@@ -5,4 +5,7 @@ for i in range(100, 1000):
         product = i * j
         if str(product) == str(product)[::-1]:
             largest_palindrome = max(largest_palindrome, product)
-print(largest_palindrome)
+            largest_palindrome=product;
+
+with open("102-result" , "w")as file:
+file.write(str(largest_palindrome))
