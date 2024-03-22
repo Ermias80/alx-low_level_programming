5274631351df2b699277a746cf4790a10b902331
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-21 18:19:23 -0700

    all

---
+++ 0x17-doubly_linked_lists/102-result.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+largest_palindrome = 0
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if str(product) == str(product)[::-1]:
+            largest_palindrome = max(largest_palindrome, product)
+print(largest_palindrome)
