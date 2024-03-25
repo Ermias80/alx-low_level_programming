1fbda31b17e1994db817723d24f40f0c020b3f74
Ermias80 <ermiasmaz8069@gmail.com>
2024-03-25 14:36:55 -0700

    al

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ * sum - addition of two numbers
+ * @a: the value of A
+ * @b: the value of b
+ * return: Addition
+ */
+int sum(int a, int b)
+{
+	return(a + b);
+}
+
+#include <stdio.h>
+/**
+ * sub - subtrion of two numbers
+ * @a: the value of A
+ * @b: the value of b
+ * return: subtraction
+ */
+int sub(int a, int b)
+{
+	return(a - b);
+}
+
+#include <stdio.h>
+/**
+ * mul - mul of two numbers
+ * @a: the value of A
+ * @b: the value of b
+ * return: Addition
+ */
+int mul(int a, int b)
+{
+        return(a + b);
+}
+
+#include <stdio.h>
+/**
+ * div - dev of two numbers
+ * @a: the value of A
+ * @b: the value of b
+ * return: divishon
+ */
+int div(int a, int b)
+{
+        return(a / b);
+}
+
+#include <stdio.h>
+/**
+ * moud - modulation of two numbers
+ * @a: the value of A
+ * @b: the value of b
+ * return: modulation
+ */
+int moud(int a, int b)
+{
+        return(a % b);
+}
